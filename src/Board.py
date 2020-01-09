0979975253024c4e8c7349ade8ababdccdae95c3
Urmzd <urmzds@gmail.com>
2020-01-09 12:05:12 -0400

    Add comments to class and constructor.

--- src/Board.py
+++ src/Board.py
@@ -1,12 +1,20 @@
 import Pieces.py
 
+"""
+    @desc: The Board class holds all the pieces in the game and contains methods to 
+    update the position of the pieces and attributes to hold information for rules such
+    as the 50-move rule and threefold-repetition rule.
+"""
 class Board:
 
-    def __init__(self, board):
-        self.board = []
-        self.counter = 0
-        self.lastMove = ""
-
+    """
+        @desc: A constructor which create a board for the chess pieces to move on.
+    """
+    def __init__(self):
+        self.board = [] # A 8 x 8 board.
+        self.counter = 0 # Number of moves since the last capture or pawn move.
+        self.lastMove = "" # A string to hold the last move made in string format.
+    
     def move(self, x1, y1, x2, y2):
         board[x1][y1].move(x2, y2)
 
