45e7b26563d8fe30d2298e40d112b8af7b9759fd
Urmzd <urmzds@gmail.com>
2020-01-09 19:15:18 -0400

    Add pieces on board and test to see if print works.

--- src/Board.py
+++ src/Board.py
@@ -1,4 +1,10 @@
 from Piece import Piece
+from Pawn import Pawn
+from King import King
+from Knight import Knight
+from Queen import Queen
+from Rook import Rook
+from Bishop import Bishop
 
 """
     @desc: The Board class holds all the pieces in the game and contains methods to 
@@ -11,10 +17,29 @@ class Board:
         @desc: A constructor which create a board for the chess pieces to move on.
     """
     def __init__(self):
-        self.board = [] # A 8 x 8 board.
+        self.board = [[None for x in range(8)] for y in range(8)] # A 8 x 8 board.
         self.counter = 0 # Number of moves since the last capture or pawn move.
         self.lastMove = "" # A string to hold the last move made in string format.
     
+    def fillBoard(self):
+        
+        for x in range(8):
+            self.board[1][x] = Pawn("W", x, 1, self.board)
+            self.board[6][x] = Pawn("B", x, 6, self.board)
+
+        colors = ["W", "B"]
+        yLocations = [0, 7]
+
+        for i in range(2):
+            self.board[yLocations[i]][0] = Rook(colors[i], 0, yLocations[i], self.board)
+            self.board[yLocations[i]][1] = Knight(colors[i], 1, yLocations[i], self.board)
+            self.board[yLocations[i]][2] = Bishop(colors[i], 2, yLocations[i], self.board)
+            self.board[yLocations[i]][3] = Queen(colors[i], 3, yLocations[i], self.board)
+            self.board[yLocations[i]][4] = King(colors[i], 4, yLocations[i], self.board)
+            self.board[yLocations[i]][5] = Bishop(colors[i], 5, yLocations[i], self.board)
+            self.board[yLocations[i]][6] = Knight(colors[i], 6, yLocations[i], self.board)
+            self.board[yLocations[i]][7] = Rook(colors[i], 7, yLocations[i], self.board)
+            
     """
         @desc: move takes a piece at (x1, y2) and moves it to (x2, y2) as long as the move is valid.
         @param x1: The x location of the piece to be moved.
@@ -23,25 +48,54 @@ class Board:
         @param y2: The y location the piece is being requested to move to.
     """
     def move(self, x1, y1, x2, y2):
-        board[x1][y1].move(x2, y2)
+        self.board[x1][y1].move(x2, y2)
 
     """
-        @desc: Converts lower case alphabets in the ASCII set to a integer.
+        @desc: convertAlpha converts lower case alphabets in the ASCII set to a integer.
         @param letter: The ASCII letter to convert.
+        @param case: True for upper case letters, false for lower case letters.
     """
-    def convertY(self, letter):
-        return ord(chr(letter)) - 96
+    def convertAlpha(self, letter, case):
+        if case:
+            return ord(chr(letter)) - 65
+        else:
+            return ord(chr(letter) - 97)
+
+    """
+        @desc: convertNumber converts number to alphabet form the ASCII set.
+        @param number: The number to convert.
+        @param case: True for upper case letters, false for lower case letters.
+    """
+    def convertNumber(self, number, case):
+        if case:
+            return chr(number + 65)
+        else:
+            return chr(number + 97)
 
     """
         @desc: Prints all pieces on the board.
     """
     def printBoard(self):
+
+        line = "" # Line to print.
+        topLine = "  1 2 3 4 5 6 7 8"
+
+        print(topLine + "\n")
         # Iterates through the board and prints each piece.
         for x in range(8):
-            print("\n") # New line for every 8 pieces printed.
-            for y in range(8):
-                if Board[x][y] == None:
-                    print("-") # In case no piece exists at unit.
-                else:
-                    print(Board[x][y])
 
+            line = line + self.convertNumber(x, True)
+
+            for y in range(8):
+                if self.board[x][y] == None:
+                    line = line + " " + "-" # In case no piece exists at unit.
+                else:
+                    line = line + " " + repr(self.board[x][y])
+            print(line + "\n") # New line for every 8 pieces printed.
+            line = ""
+
+
+
+board = Board()
+board.fillBoard()
+board.printBoard()
