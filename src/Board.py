b72402f04e00b61b5eeb6fc943602cc816fca087
Urmzd <urmzds@gmail.com>
2020-01-09 12:10:44 -0400

    Add comments to methods.

--- src/Board.py
+++ src/Board.py
@@ -1,4 +1,4 @@
-import Pieces.py
+import Piece
 
 """
     @desc: The Board class holds all the pieces in the game and contains methods to 
@@ -15,11 +15,22 @@ class Board:
         self.counter = 0 # Number of moves since the last capture or pawn move.
         self.lastMove = "" # A string to hold the last move made in string format.
     
+    """
+        @desc: move takes a piece at (x1, y2) and moves it to (x2, y2) as long as the move is valid.
+        @param x1: The x location of the piece to be moved.
+        @param y1: The y location of the piece to be moved.
+        @param x2: The x location the piece is being requested to move to.
+        @param y2: The y location the piece is being requested to move to.
+    """
     def move(self, x1, y1, x2, y2):
         board[x1][y1].move(x2, y2)
 
-    def convertY(self, y):
-        return ord(chr(y)) - 96
+    """
+        @desc: Converts lower case alphabets in the ASCII set to a integer.
+        @param letter: The ASCII letter to convert.
+    """
+    def convertY(self, letter):
+        return ord(chr(letter)) - 96
 
     def __str__(self):
         pass
