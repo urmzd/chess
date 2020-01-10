a36df2eaee26289dad26cbf8bd7e5a49c59d0ede
Urmzd <urmzds@gmail.com>
2020-01-10 19:17:20 -0400

    Remove syntax error in constructor.

--- src/Rook.py
+++ src/Rook.py
@@ -2,6 +2,6 @@ from Piece import Piece
 
 class Rook(Piece):
 
-    def __init__(self, color, x, y, board, played):
+    def __init__(self, color, x, y, board):
         super().__init__("R", color, 5, x, y, board)
         self.played = False
